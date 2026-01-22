60c9dd32e09a68752687a976b9f34c1ddef216e5
aKARNWAL <154620095+aKARNWAL@users.noreply.github.com>
2026-01-22 11:16:49 +0530

    add basic rounting for admin section

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppBar from "./components/AppBar"
+import Drawer from "./components/Drawer"
+import Home from "./pages/Home";
+import Teachers from "./pages/Teachers";
+import Students from "./pages/Students";
+import Notices from "./pages/Notices";
+
+function App() {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <AppBar setOpen = {setOpen}/>
+      <Drawer open = {open} setOpen = {setOpen}/>
+      {/* <Navbar /> */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/teacher" element={<Teachers />} />
+        <Route path="/student" element={<Students />} />
+        <Route path="/notices" element={<Notices />} />
+      </Routes>
+     </>
+  )
+}
+
+export default App
---
+++ src/components/Drawer.jsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { Link } from "react-router-dom";
+import Students from '../pages/Students';
+import Teachers from '../pages/Teachers'
+import Notices from '../pages/Notices'
+import './Drawer.css';
+
+
+const routeMap = {
+    home: '/',
+    students: '/student',
+    teachers:'/teacher',
+    notices:'/notices'
+}
+export default function AnchorTemporaryDrawer(props) {
+  const {open, setOpen} = props
+  // const [openDrawer, setOpenDrawer] = React.useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setOpenDrawer(open);
+  };
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={() => setOpen(false)}
+      onKeyDown={() => setOpen(false)}
+    >
+      <List>
+        {['Home', 'Teachers', 'Students', 'Notices'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <Link to={routeMap[text.toLowerCase()] || '/'}>{text}</Link>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>      
+          <Drawer
+            anchor={"left"}
+            open={open}
+            onClose={() => setOpen(false)}
+            >
+            {list()}
+          </Drawer>
+    </div>
+  );
+}
